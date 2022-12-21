48fd870f853e5e65e9645d9685ba8dd40fe1799c
Midland707 <rus14job@ukr.net>
2022-12-21 21:39:09 +0200

    add libs and tasks

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,44 @@
+// Завдання 3 - генератор промісів
+
+// HTML містить розмітку форми, в поля якої користувач буде вводити першу затримку в мілісекундах,
+//   крок збільшення затримки для кожного промісу після першого і кількість промісів, яку необхідно створити.
+
+// <form class="form">
+//   <label>
+//     First delay (ms)
+//     <input type="number" name="delay" required />
+//   </label>
+//   <label>
+//     Delay step (ms)
+//     <input type="number" name="step" required />
+//   </label>
+//   <label>
+//     Amount
+//     <input type="number" name="amount" required />
+//   </label>
+//   <button type="submit">Create promises</button>
+// </form>
+
+//   Напиши скрипт, який на момент сабміту форми викликає функцію createPromise(position, delay) стільки разів, скільки ввели в поле amount.
+// Під час кожного виклику передай їй номер промісу(position), що створюється, і затримку, враховуючи першу затримку(delay),
+//   введену користувачем, і крок(step).
+
+// Доповни код функції createPromise таким чином, щоб вона повертала один проміс,
+//   який виконується або відхиляється через delay часу.Значенням промісу повинен бути об'єкт,
+//   в якому будуть властивості position і delay зі значеннями однойменних параметрів.
+//   Використовуй початковий код функції для вибору того, що потрібно зробити з промісом - виконати або відхилити.
+
+// createPromise(2, 1500)
+//   .then(({ position, delay }) => {
+//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//   })
+//   .catch(({ position, delay }) => {
+//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+//   });
+
+// all modules
+import Notiflix from 'notiflix';
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
@@ -6,3 +47,12 @@ function createPromise(position, delay) {
     // Reject
   }
 }
+
+// e.g. Only message
+Notiflix.Notify.success('Sol lucet omnibus');
+
+Notiflix.Notify.failure('Qui timide rogat docet negare');
+
+Notiflix.Notify.warning('Memento te hominem esse');
+
+Notiflix.Notify.info('Cogito ergo sum');
