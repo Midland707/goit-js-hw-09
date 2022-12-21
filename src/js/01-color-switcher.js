56a1718143725bd2299364a7546a5ec906ab6a1c
Midland707 <rus14job@ukr.net>
2022-12-21 22:00:49 +0200

    add task-01 v.1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -15,3 +15,25 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyStyles = document.querySelector('body');
+const buttonStart = document.querySelector(`[data-start]`);
+const buttonStop = document.querySelector(`[data-stop]`);
+let timerId = null;
+
+function startFunction() {
+  console.log('Start');
+  timerId = setInterval(() => {
+    const color = getRandomHexColor();
+    bodyStyles.style.backgroundColor = color;
+    console.log(`Random color =${color}`);
+  }, 1000);
+}
+
+function stopFunction() {
+  console.log('Stop');
+  clearInterval(timerId);
+}
+
+buttonStart.addEventListener('click', startFunction);
+buttonStop.addEventListener('click', stopFunction);
