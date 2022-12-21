6ffc485f1ca6ac33c6563d52a8d06646813d7c39
Midland707 <109915910+Midland707@users.noreply.github.com>
2022-12-21 20:01:14 +0000

    Deploying to gh-pages from @ Midland707/goit-js-hw-09@56a1718143725bd2299364a7546a5ec906ab6a1c 🚀

---
+++ 01-color-switcher.574cd778.js
@@ -0,0 +1,2 @@
+const o=document.querySelector("body"),t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");let n=null;t.addEventListener("click",(function(){console.log("Start"),n=setInterval((()=>{const t=`#${Math.floor(16777215*Math.random()).toString(16)}`;o.style.backgroundColor=t,console.log(`Random color =${t}`)}),1e3)})),e.addEventListener("click",(function(){console.log("Stop"),clearInterval(n)}));
+//# sourceMappingURL=01-color-switcher.574cd778.js.map
